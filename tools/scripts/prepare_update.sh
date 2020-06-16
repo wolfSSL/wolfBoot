b3fad42f054f6e05473a08ff4e91448275c6f9a4
Daniele Lacamera <dan@danielinux.net>
2020-06-16 13:32:06 +0200

    Added helper scripts to create test update images, config for enc-uart use case

---
+++ tools/scripts/prepare_update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# SIZE is WOLFBOOT_PARTITION_SIZE - 5
+SIZE=131067
+VERSION=8
+APP=test-app/image_v"$VERSION"_signed.bin
+./tools/keytools/sign.py test-app/image.bin ecc256.der $VERSION
+dd if=/dev/zero bs=$SIZE count=1 2>/dev/null | tr "\000" "\377" > update.bin
+dd if=$APP of=update.bin bs=1 conv=notrunc
+printf "pBOOT"  >> update.bin
+
