d5008c05899ebe4a9ceca5182b41a9d7724cccb7
Liam <liam.oorts@aloxy.io>
2020-09-21 15:35:23 +0200

    increase spi speed

--- hal/spi/spi_drv_stm32.c
+++ hal/spi/spi_drv_stm32.c
@@ -155,7 +155,7 @@ void spi_init(int polarity, int phase)
         spi_tpm2_pin_setup();
         APB2_CLOCK_ER |= SPI1_APB2_CLOCK_ER_VAL;
         spi1_reset();
-        SPI1_CR1 = SPI_CR1_MASTER | (5 << 3) | (polarity << 1) | (phase << 0);
+        SPI1_CR1 = SPI_CR1_MASTER | (polarity << 1) | (phase << 0);
         SPI1_CR2 |= SPI_CR2_SSOE;
         SPI1_CR1 |= SPI_CR1_SPI_EN;
     }
