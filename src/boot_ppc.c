052b332ef28b4c1a1f0756f739a65d03a460c585
Elms <jeff@wolfssl.com>
2021-06-03 21:38:06 -0700

    PPC-t2080: Add PPC and NXP T2080 startup

---
+++ src/boot_ppc.c
@@ -0,0 +1,35 @@
+#include <stdint.h>
+
+#include "image.h"
+#include "loader.h"
+#include "wolfboot/wolfboot.h"
+
+extern unsigned int __bss_start__;
+extern unsigned int __bss_end__;
+
+extern void main(void);
+
+void boot_entry_C(void)
+{
+    register unsigned int *dst;
+    /* Initialize the BSS section to 0 */
+    dst = &__bss_start__;
+    while (dst < (unsigned int *)&__bss_end__) {
+        *dst = 0U;
+        dst++;
+    }
+
+    /* Run wolfboot! */
+    main();
+}
+
+#ifdef MMU
+void do_boot(const uint32_t *app_offset, const uint32_t* dts_offset)
+#else
+void do_boot(const uint32_t *app_offset)
+#endif
+{
+  asm volatile("mtlr %0; blr":: "r"(app_offset));
+}
+
+void arch_reboot(void) {}
