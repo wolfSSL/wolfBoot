c33a67910181ea077cbb1e383527418b39f4933d
Daniele Lacamera <dan@danielinux.net>
2021-07-09 18:27:49 +0200

    Reworked after reviewer's comments

--- src/xmalloc.c
+++ src/xmalloc.c
@@ -26,7 +26,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <wolfssl/wolfcrypt/settings.h>
 #include <wolfssl/wolfcrypt/sp.h>
+#include <wolfssl/wolfcrypt/sp_int.h>
 #include "target.h"
 
 
@@ -36,7 +38,7 @@ struct xmalloc_slot {
     uint32_t in_use;
 };
 
-#define SP_DIGIT_SIZE (4)
+#define SP_DIGIT_SIZE (sizeof(sp_digit))
 
 #ifdef WOLFBOOT_HASH_SHA256
 #   include <wolfssl/wolfcrypt/sha256.h>
@@ -49,32 +51,36 @@ struct xmalloc_slot {
 #endif
 
 #ifdef WOLFBOOT_SIGN_ECC256
-#define SP_CURVE_SPECS_SIZE (80)
-#ifdef WOLFSSL_SP_ARM_CORTEX_M_ASM
-    #define SP_POINT_SIZE (196)
-    #define SP_DIGITS_BUFFER_SIZE_0 (SP_DIGIT_SIZE * 16 * 8)
-    #define SP_DIGITS_BUFFER_SIZE_1 (SP_DIGIT_SIZE * 2 * 8 * 5)
-#else
-    #define SP_POINT_SIZE (244)
-    #define SP_DIGITS_BUFFER_SIZE_0 (SP_DIGIT_SIZE * 16 * 10)
-    #define SP_DIGITS_BUFFER_SIZE_1 (SP_DIGIT_SIZE * (3 * 10 + 1))
-    #define SP_DIGITS_BUFFER_SIZE_2 (SP_DIGIT_SIZE * (2 * 10 * 5))
-#endif
+    #define SP_CURVE_SPECS_SIZE (80)
+    #ifdef WOLFSSL_SP_ARM_CORTEX_M_ASM
+        #define SP_POINT_SIZE (196)
+        #define SP_DIGITS_BUFFER_SIZE_0 (SP_DIGIT_SIZE * 16 * 8)
+        #define SP_DIGITS_BUFFER_SIZE_1 (SP_DIGIT_SIZE * 2 * 8 * 5)
+    #else
+        #define SP_POINT_SIZE (244)
+        #define SP_DIGITS_BUFFER_SIZE_0 (SP_DIGIT_SIZE * 16 * 10)
+        #define SP_DIGITS_BUFFER_SIZE_1 (SP_DIGIT_SIZE * (3 * 10 + 1))
+        #define SP_DIGITS_BUFFER_SIZE_2 (SP_DIGIT_SIZE * (2 * 10 * 5))
+    #endif
+
 static uint8_t sp_curve_specs[SP_CURVE_SPECS_SIZE];
 static uint8_t sp_points_0[SP_POINT_SIZE * 2];
 static uint8_t sp_points_1[SP_POINT_SIZE * 2];
 static uint8_t sp_points_2[SP_POINT_SIZE * (16 + 1)];
+
 #ifndef WC_NO_CACHE_RESISTANT
 static uint8_t sp_points_3[SP_POINT_SIZE];
 #endif
+
 static uint8_t sp_digits_buffer_0[SP_DIGITS_BUFFER_SIZE_0];
 static uint8_t sp_digits_buffer_1[SP_DIGITS_BUFFER_SIZE_1];
+
 #ifndef WOLFSSL_SP_ARM_CORTEX_M_ASM
 static uint8_t sp_digits_buffer_2[SP_DIGITS_BUFFER_SIZE_2];
 static uint8_t sp_montgomery[sizeof(int64_t) * 2 * 8];
 #endif
-static uint32_t sha_block[HASH_BLOCK_SIZE];
 
+static uint32_t sha_block[HASH_BLOCK_SIZE];
 static struct xmalloc_slot xmalloc_pool[] = {
 #ifdef WOLFBOOT_HASH_SHA256
     { (uint8_t *)sha_block, HASH_BLOCK_SIZE * sizeof(uint32_t), 0 },
@@ -98,7 +104,6 @@ static struct xmalloc_slot xmalloc_pool[] = {
 };
 
 #elif defined WOLFBOOT_SIGN_ED25519
-
 static uint32_t sha_block[HASH_BLOCK_SIZE];
 static uint32_t sha512_block[sizeof(word64) * 16];
 
@@ -110,57 +115,46 @@ static struct xmalloc_slot xmalloc_pool[] = {
     { NULL, 0, 0}
 };
 
-#elif defined WOLFBOOT_SIGN_RSA2048
-    #ifdef WOLFSSL_SP_ARM_CORTEX_M_ASM
-        #define SPDIGIT_BUF0_SIZE (SP_DIGIT_SIZE * 64 * 5)
+#elif defined(WOLFBOOT_SIGN_RSA2048) || defined(WOLFBOOT_SIGN_RSA4096)
+    #ifdef WOLFBOOT_SIGN_RSA2048
+        #ifdef WOLFSSL_SP_ARM_CORTEX_M_ASM
+            #define SPDIGIT_BUF0_SIZE (SP_DIGIT_SIZE * 64 * 5)
+        #else
+            #define SPDIGIT_BUF0_SIZE (SP_DIGIT_SIZE * 90 * 5)
+            #define SPDIGIT_BUF1_SIZE (SP_DIGIT_SIZE * (90 * 4 + 3))
+            static uint8_t sp_digit_buf1[SPDIGIT_BUF1_SIZE];
+        #endif
     #else
-        #define SPDIGIT_BUF0_SIZE (SP_DIGIT_SIZE * 90 * 5)
-        #define SPDIGIT_BUF1_SIZE (SP_DIGIT_SIZE * (90 * 4 + 3))
-        static uint8_t sp_digit_buf1[SPDIGIT_BUF1_SIZE];
+        #ifdef WOLFSSL_SP_ARM_CORTEX_M_ASM
+            #define SPDIGIT_BUF0_SIZE (SP_DIGIT_SIZE * 128 * 5)
+        #else
+            #define SPDIGIT_BUF0_SIZE (SP_DIGIT_SIZE * 180 * 5)
+            #define SPDIGIT_BUF1_SIZE (SP_DIGIT_SIZE * (180 * 4 + 3))
+            static uint8_t sp_digit_buf1[SPDIGIT_BUF1_SIZE];
+        #endif
     #endif
-    static uint32_t sha_block[HASH_BLOCK_SIZE];
-    static uint8_t sp_digit_buf0[SPDIGIT_BUF0_SIZE];
-    static struct xmalloc_slot xmalloc_pool[] = {
-    #ifdef WOLFBOOT_HASH_SHA256
-        { (uint8_t *)sha_block, WC_SHA256_BLOCK_SIZE * sizeof(uint32_t), 0 },
-    #endif
-        { sp_digit_buf0, SPDIGIT_BUF0_SIZE, 0},
-    #ifndef WOLFSSL_SP_ARM_CORTEX_M_ASM
-        { sp_digit_buf1, SPDIGIT_BUF1_SIZE, 0},
-    #endif
-        { NULL, 0, 0}
-    };
 
-#elif defined WOLFBOOT_SIGN_RSA4096
-    #ifdef WOLFSSL_SP_ARM_CORTEX_M_ASM
-        #define SPDIGIT_BUF0_SIZE (SP_DIGIT_SIZE * 128 * 5)
-    #else
-        #define SPDIGIT_BUF0_SIZE (SP_DIGIT_SIZE * 180 * 5)
-        #define SPDIGIT_BUF1_SIZE (SP_DIGIT_SIZE * (180 * 4 + 3))
-        static uint8_t sp_digit_buf1[SPDIGIT_BUF1_SIZE];
-    #endif
-    static uint32_t sha_block[HASH_BLOCK_SIZE];
-    static uint8_t sp_digit_buf0[SPDIGIT_BUF0_SIZE];
-    static struct xmalloc_slot xmalloc_pool[] = {
-    #ifdef WOLFBOOT_HASH_SHA256
-        { (uint8_t *)sha_block, WC_SHA256_BLOCK_SIZE * sizeof(uint32_t), 0 },
-    #endif
-        { sp_digit_buf0, SPDIGIT_BUF0_SIZE, 0},
-    #ifndef WOLFSSL_SP_ARM_CORTEX_M_ASM
-        { sp_digit_buf1, SPDIGIT_BUF1_SIZE, 0},
-    #endif
-        { NULL, 0, 0}
-    };
-
+static uint32_t sha_block[HASH_BLOCK_SIZE];
+static uint8_t sp_digit_buf0[SPDIGIT_BUF0_SIZE];
+static struct xmalloc_slot xmalloc_pool[] = {
+#ifdef WOLFBOOT_HASH_SHA256
+    { (uint8_t *)sha_block, WC_SHA256_BLOCK_SIZE * sizeof(uint32_t), 0 },
+#endif
+    { sp_digit_buf0, SPDIGIT_BUF0_SIZE, 0},
+#ifndef WOLFSSL_SP_ARM_CORTEX_M_ASM
+    { sp_digit_buf1, SPDIGIT_BUF1_SIZE, 0},
+#endif
+    { NULL, 0, 0}
+};
 
 #elif defined WOLFBOOT_NO_SIGN
-    static uint32_t sha_block[HASH_BLOCK_SIZE];
-    static struct xmalloc_slot xmalloc_pool[] = {
-    #ifdef WOLFBOOT_HASH_SHA256
-        { (uint8_t *)sha_block, WC_SHA256_BLOCK_SIZE * sizeof(uint32_t), 0 },
-    #endif
-        { NULL, 0, 0}
-    };
+static uint32_t sha_block[HASH_BLOCK_SIZE];
+static struct xmalloc_slot xmalloc_pool[] = {
+#ifdef WOLFBOOT_HASH_SHA256
+    { (uint8_t *)sha_block, WC_SHA256_BLOCK_SIZE * sizeof(uint32_t), 0 },
+#endif
+    { NULL, 0, 0}
+};
 
 #else 
 #   error "No cipher selected."
@@ -172,7 +166,7 @@ void* XMALLOC(size_t n, void* heap, int type)
 
     while (xmalloc_pool[i].addr) {
         if ((n == xmalloc_pool[i].size) &&
-            (xmalloc_pool[i].in_use == 0)) {
+                (xmalloc_pool[i].in_use == 0)) {
             xmalloc_pool[i].in_use++;
             return xmalloc_pool[i].addr;
         }
