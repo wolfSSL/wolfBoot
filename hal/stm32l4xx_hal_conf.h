36936a078ff80f2abaeffd09adbc055611c8cd85
Hajer KALLEL <hajerkallel5@gmail.com>
2021-08-09 14:29:17 +0100

    stm32l4xx hal configuration

---
+++ hal/stm32l4xx_hal_conf.h
@@ -0,0 +1,9 @@
+#ifndef __STM32L4xx_HAL_CONF_H
+#define __STM32L4xx_HAL_CONF_H
+#define HAL_MODULE_ENABLED  
+#define HAL_FLASH_MODULE_ENABLED
+#define HAL_RCC_MODULE_ENABLED
+#include "stm32l4xx_hal_flash.h"
+#include "stm32l4xx_hal_rcc.h"
+#define assert_param(expr) ((void)0U)
+#endif /* __STM32L4xx_HAL_CONF_H */
