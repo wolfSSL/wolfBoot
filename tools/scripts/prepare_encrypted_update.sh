8ffdf1997be5e67bd2a23b117b3a56a2429ad84b
Daniele Lacamera <dan@danielinux.net>
2021-10-07 12:25:20 +0200

    Fixed scripts by adding explicit "--ecc256" argument

--- tools/scripts/prepare_encrypted_update.sh
+++ tools/scripts/prepare_encrypted_update.sh
@@ -13,7 +13,7 @@ APP=test-app/image_v"$VERSION"_signed_and_encrypted.bin
 # Create test key
 echo -n "0123456789abcdef0123456789abcdef0123456789ab" > enc_key.der
 
-$SIGN_TOOL --encrypt enc_key.der test-app/image.bin ecc256.der $VERSION
+$SIGN_TOOL --ecc256 --encrypt enc_key.der test-app/image.bin ecc256.der $VERSION
 dd if=/dev/zero bs=$SIZE count=1 2>/dev/null | tr "\000" "\377" > update.bin
 dd if=$APP of=update.bin bs=1 conv=notrunc
 printf "pBOOT"  >> update.bin
