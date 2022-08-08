f43d6dc47e74b371075a706878e82d99cc276c9a
Takashi Kojo <kojo@wolfssl.com>
2022-08-09 07:25:44 +0900

    Initial wolfBoot project, Renesas RX72N, e2Studio

---
+++ IDE/Renesas/e2studio_CCRX/wolfBoot/src/wolfBoot.c
@@ -0,0 +1,17 @@
+/***********************************************************************
+*
+*  FILE        : wolfBoot.c
+*  DATE        : 2022-08-09
+*  DESCRIPTION : Main Program
+*
+*  NOTE:THIS IS A TYPICAL EXAMPLE.
+*
+***********************************************************************/
+#include "r_smc_entry.h"
+
+void main(void);
+
+void main(void)
+{
+
+}
