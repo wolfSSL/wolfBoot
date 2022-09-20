03eb6b36dd3a542bc68d7517cb27e9bab31ac1df
Takashi Kojo <kojo@wolfssl.com>
2022-09-21 05:37:45 +0900

    Add tools/format-conv/der2raw

---
+++ tools/format-conv/user_settings.h
@@ -0,0 +1,15 @@
+#define HAVE_ECC
+
+#define ECC_TIMING_RESISTANT
+#define WC_RSA_BLINDING
+
+#define WOLFSSL_HAVE_SP_RSA
+#define WOLFSSL_HAVE_SP_ECC
+#define WOLFSSL_SP
+
+#define NO_SHA
+#define NO_MD5
+#define NO_DES3
+#define NO_DSA
+#define NO_OLD_TLS
+#define WOLFCRYPT_ONLY
