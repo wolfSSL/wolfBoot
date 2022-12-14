wolfSSL/wolfBoot

commit cd6597e2a07af6c35b0d156611d4a26ea90bbdea

Fix bug with fixed partitions not setting `hdr` and `trailer` correctly.

src/image.c
@@ -786,16 +786,14 @@ uint32_t wolfBoot_image_size(uint8_t *image)
 
 int wolfBoot_open_image_address(struct wolfBoot_image* img, uint8_t* image)
 {
-    uint32_t *magic;
-
-
-    magic = (uint32_t *)(image);
+    uint32_t *magic = (uint32_t *)(image);
     if (*magic != WOLFBOOT_MAGIC) {
         wolfBoot_printf("Boot header magic 0x%08x invalid at %p\n",
             *magic, image);
         return -1;
     }
     img->fw_size = wolfBoot_image_size(image);
+    wolfBoot_printf("Part: Size %d\n", img->fw_size);
 #ifdef WOLFBOOT_FIXED_PARTITIONS
     if (img->fw_size > (WOLFBOOT_PARTITION_SIZE - IMAGE_HEADER_SIZE)) {
         wolfBoot_printf("Image size %d > max %d\n",
@@ -803,13 +801,13 @@ int wolfBoot_open_image_address(struct wolfBoot_image* img, uint8_t* image)
         img->fw_size = 0;
         return -1;
     }
-    img->trailer = img->hdr + WOLFBOOT_PARTITION_SIZE;
-#else
-    /* This function was called directly, img->hdr is not yet set */
-    img->hdr = image;
 #endif
-    img->hdr_ok = 1;
+    if (!img->hdr_ok) {
+        img->hdr = image;
+        img->hdr_ok = 1;
+    }
     img->fw_base = img->hdr + IMAGE_HEADER_SIZE;
+    img->trailer = img->hdr + WOLFBOOT_PARTITION_SIZE;
 
     return 0;
 }
@@ -861,8 +859,8 @@ int wolfBoot_open_image(struct wolfBoot_image *img, uint8_t part)
     memset(img, 0, sizeof(struct wolfBoot_image));
     img->part = part;
     if (part == PART_SWAP) {
-        img->hdr_ok = 1;
         img->hdr = (void*)WOLFBOOT_PARTITION_SWAP_ADDRESS;
+        img->hdr_ok = 1;
         img->fw_base = img->hdr;
         img->fw_size = WOLFBOOT_SECTOR_SIZE;
         return 0;
@@ -888,11 +886,14 @@ int wolfBoot_open_image(struct wolfBoot_image *img, uint8_t part)
     if (part == PART_BOOT) {
         img->hdr = (void*)WOLFBOOT_PARTITION_BOOT_ADDRESS;
         wolfBoot_printf("Boot partition: %p\n", img->hdr);
-    } else if (part == PART_UPDATE) {
+    }
+    else if (part == PART_UPDATE) {
         img->hdr = (void*)WOLFBOOT_PARTITION_UPDATE_ADDRESS;
         wolfBoot_printf("Update partition: %p\n", img->hdr);
-    } else
+    }
+    else {
         return -1;
+    }
 
     /* fetch header address
      * (or copy from external device to a local buffer via fetch_hdr_cpy)

commit 9bc73ce422d7c976f92470211ec3a8d73efe4674

Fix for external SPI broken with https://github.com/wolfSSL/wolfBoot/pull/242/commits/cd6597e2a07af6c35b0d156611d4a26ea90bbdea

hal/stm32f4.c
@@ -20,7 +20,9 @@
  */
 
 #include <stdint.h>
-#include <image.h>
+
+#include "image.h"
+#include "spi_drv.h"
 
 #ifndef ARCH_FLASH_OFFSET
 #define ARCH_FLASH_OFFSET 0x08000000U
@@ -60,10 +62,6 @@
 #define APB1_CLOCK_RST          (*(volatile uint32_t *)(0x40023820U))
 #define TIM2_APB1_CLOCK_ER_VAL     (1 << 0)
 #define PWR_APB1_CLOCK_ER_VAL   (1 << 28)
-
-#define APB2_CLOCK_ER           (*(volatile uint32_t *)(0x40023844U))
-#define APB2_CLOCK_RST          (*(volatile uint32_t *)(0x40023824U))
-#define SYSCFG_APB2_CLOCK_ER (1 << 14)
 
 #define FLASH_BASE          (0x40023C00U)
 #define FLASH_ACR           (*(volatile uint32_t *)(FLASH_BASE + 0x00U))
@@ -336,6 +334,10 @@ static void clock_pll_on(int powersave)
 void hal_init(void)
 {
     clock_pll_on(0);
+
+#ifdef SPI_FLASH
+    spi_init(0,0);
+#endif
 }
 
 void hal_prepare_boot(void)

src/image.c
@@ -793,7 +793,7 @@ int wolfBoot_open_image_address(struct wolfBoot_image* img, uint8_t* image)
         return -1;
     }
     img->fw_size = wolfBoot_image_size(image);
-    wolfBoot_printf("Part: Size %d\n", img->fw_size);
+    wolfBoot_printf("Image size %d\n", img->fw_size);
 #ifdef WOLFBOOT_FIXED_PARTITIONS
     if (img->fw_size > (WOLFBOOT_PARTITION_SIZE - IMAGE_HEADER_SIZE)) {
         wolfBoot_printf("Image size %d > max %d\n",
@@ -801,11 +801,15 @@ int wolfBoot_open_image_address(struct wolfBoot_image* img, uint8_t* image)
         img->fw_size = 0;
         return -1;
     }
-#endif
     if (!img->hdr_ok) {
         img->hdr = image;
-        img->hdr_ok = 1;
     }
+#else
+    if (img->hdr == NULL) {
+        img->hdr = image;
+    }
+#endif
+    img->hdr_ok = 1;
     img->fw_base = img->hdr + IMAGE_HEADER_SIZE;
     img->trailer = img->hdr + WOLFBOOT_PARTITION_SIZE;
 

tools/test.mk
@@ -885,25 +885,25 @@ test-size-all:
 	make keysclean
 	make test-size SIGN=ED25519 LIMIT=11308
 	make keysclean
-	make test-size SIGN=ECC256  LIMIT=22162
+	make test-size SIGN=ECC256  LIMIT=22166
 	make keysclean
-	make test-size SIGN=ECC256 NO_ASM=1 LIMIT=13594
+	make test-size SIGN=ECC256 NO_ASM=1 LIMIT=13598
 	make keysclean
-	make test-size SIGN=RSA2048 LIMIT=11090
+	make test-size SIGN=RSA2048 LIMIT=11094
 	make keysclean
-	make test-size SIGN=RSA2048 NO_ASM=1 LIMIT=11062
+	make test-size SIGN=RSA2048 NO_ASM=1 LIMIT=11066
 	make keysclean
-	make test-size SIGN=RSA4096 LIMIT=11450
+	make test-size SIGN=RSA4096 LIMIT=11454
 	make keysclean
-	make test-size SIGN=RSA4096 NO_ASM=1 LIMIT=11370
+	make test-size SIGN=RSA4096 NO_ASM=1 LIMIT=11374
 	make keysclean
-	make test-size SIGN=ECC384 LIMIT=17500
+	make test-size SIGN=ECC384 LIMIT=17504
 	make keysclean
-	make test-size SIGN=ECC384 NO_ASM=1 LIMIT=15032
+	make test-size SIGN=ECC384 NO_ASM=1 LIMIT=15036
 	make keysclean
-	make test-size SIGN=ED448 LIMIT=13342
+	make test-size SIGN=ED448 LIMIT=13346
 	make keysclean
-	make test-size SIGN=RSA3072 LIMIT=11290
+	make test-size SIGN=RSA3072 LIMIT=11294
 	make keysclean
-	make test-size SIGN=RSA3072 NO_ASM=1 LIMIT=11162
+	make test-size SIGN=RSA3072 NO_ASM=1 LIMIT=11166
 	make keysclean