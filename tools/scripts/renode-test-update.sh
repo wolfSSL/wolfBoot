13d4ffa7e49a2a99eac6d53174d0c2e3366b5249
Daniele Lacamera <dan@danielinux.net>
2023-09-04 11:10:30 +0200

    Attempt to add git to the docker

--- tools/scripts/renode-test-update.sh
+++ tools/scripts/renode-test-update.sh
@@ -21,6 +21,7 @@ quit_renode() {
 rm -f $RENODE_UART
 
 if (echo $TEST_OPTIONS | grep "LMS" &>/dev/null); then
+    apt install -y git
     mkdir -p lib/hss-lib/lib
     git clone https://github.com/cisco/hash-sigs.git lib/hash-sigs/src
     cd lib/hash-sigs/src && git checkout b0631b8891295bf2929e68761205337b7c031726
