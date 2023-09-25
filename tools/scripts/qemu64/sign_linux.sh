e8907a7dc2e8a056d0b1e669bfa4d60a4a1c3708
Marco Oliverio <marco@wolfssl.com>
2023-09-25 08:43:42 +0000

    tools/script: use env variable for SING and HASH algos

--- tools/scripts/qemu64/sign_linux.sh
+++ tools/scripts/qemu64/sign_linux.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
+SIGN=${SIGN:-"--ecc256"}
+HASH=${HASH:-"--sha256"}
 
 cp /tmp/br-linux-wolfboot/output/images/bzImage .
-tools/keytools/sign --ecc384 --sha384 bzImage wolfboot_signing_private_key.der 8
-tools/keytools/sign --ecc384 --sha384 bzImage wolfboot_signing_private_key.der 2
+tools/keytools/sign $SIGN $HASH bzImage wolfboot_signing_private_key.der 8
+tools/keytools/sign $SIGN $HASH bzImage wolfboot_signing_private_key.der 2
 
 cp base-part-image app.bin
 dd if=bzImage_v8_signed.bin of=app.bin bs=1k seek=1024 conv=notrunc
