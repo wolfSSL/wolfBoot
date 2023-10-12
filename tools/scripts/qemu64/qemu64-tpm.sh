371c03e3984b6ff056b6cab8c9a3b4264588947b
Tesfa Mael <tesfa@wolfssl.com>
2023-10-12 09:38:02 -0700

    Enhance QEMU scripts

--- tools/scripts/qemu64/qemu64-tpm.sh
+++ tools/scripts/qemu64/qemu64-tpm.sh
@@ -1,8 +1,54 @@
 #!/bin/bash
-#
+
+
+#By default, the script will run without waiting for GDB. If you use the
+# -w parameter, it will wait for GDB to connect.
+
+# Usage:
+
+
+# For DEBUG_STAGE1 without waiting for GDB: ./qemu64-tpm.sh -d DEBUG_STAGE1
+# For DEBUG_STAGE2 without waiting for GDB (default): ./qemu64-tpm.sh
+# For DEBUG_STAGE1 with waiting for GDB: ./qemu64-tpm.sh -d DEBUG_STAGE1 -w
+# For DEBUG_STAGE2 with waiting for GDB: ./qemu64-tpm.sh -w
+
+
+# To DEBUG_STAGE1
+# $ gdb stage1/loader_stage1.elf
+# target remote :1234
+# b start
+# c
+
+# To DEBUG_STAGE2
+# $ gdb wolfboot.elf
+# target remote :1234
+# b main
+# c
+
 
 ## TPM emulator:
 # https://github.com/stefanberger/swtpm
+# sudo apt install swtpm
+
+# Default values
+DEBUG_STAGE="DEBUG_STAGE2"
+WAIT_FOR_GDB=false
+
+# Parse command line options
+while getopts "d:w" opt; do
+    case "$opt" in
+        d)
+            DEBUG_STAGE="$OPTARG"
+            ;;
+        w)
+            WAIT_FOR_GDB=true
+            ;;
+        *)
+            echo "Usage: $0 [-d DEBUG_STAGE1 | DEBUG_STAGE2] [-w]"
+            exit 1
+            ;;
+    esac
+done
 
 if (test -z $OVMF_PATH); then
     if (test -f /usr/share/edk2-ovmf/x64/OVMF.fd); then
@@ -24,10 +70,15 @@ QEMU_OPTIONS=" \
     -pflash wolfboot_stage1.bin -drive id=mydisk,format=raw,file=app.bin,if=none \
     -device ide-hd,drive=mydisk"
 
-QEMU=qemu-system-x86_64
-if [ "$DEBUG" = "1" ]; then
+# If waiting for GDB is true, append options to QEMU_OPTIONS
+if [ "$WAIT_FOR_GDB" = true ]; then
     QEMU_OPTIONS="${QEMU_OPTIONS} -S -s"
+fi
+
+if [ "$DEBUG_STAGE" = "DEBUG_STAGE1" ]; then
     QEMU=qemu-system-i386
+else
+    QEMU=qemu-system-x86_64
 fi
 
 killall swtpm
@@ -39,5 +90,5 @@ swtpm socket --tpm2 --tpmstate dir=/tmp/swtpm \
 sleep .5
 echo Running QEMU...
 
+echo "$QEMU $QEMU_OPTIONS $QEMU_TPM_OPTIONS"
 $QEMU $QEMU_OPTIONS $QEMU_TPM_OPTIONS
-
