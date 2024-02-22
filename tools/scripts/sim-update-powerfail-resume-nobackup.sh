bfe9e4fdf9fb2a7ae8b47584803a068d7e28d9a9
Daniele Lacamera <dan@danielinux.net>
2024-02-22 16:27:59 +0100

    Added update+powerfail test for DISABLE_BACKUP

---
+++ tools/scripts/sim-update-powerfail-resume-nobackup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+V=`./wolfboot.elf update_trigger get_version 2>/dev/null`
+if [ "x$V" != "x1" ]; then
+    echo "Failed first boot with update_trigger"
+    exit 1
+fi
+
+./wolfboot.elf powerfail 0 get_version 2>/dev/null
+./wolfboot.elf powerfail 15000 get_version 2>/dev/null
+./wolfboot.elf powerfail 18000 get_version 2>/dev/null
+./wolfboot.elf powerfail 1a000 get_version 2>/dev/null
+# fail on the last sector to stop the encrypt key save and state update
+./wolfboot.elf powerfail 3e000 get_version 2>/dev/null
+# may not trigger on non NVM_FLASH_WRITEONCE
+V=`./wolfboot.elf powerfail 3f000 get_version` 2>/dev/null
+if [ "x$V" != "x2" ]; then
+    V=`./wolfboot.elf get_version 2>/dev/null`
+    # if we failed on the final boot state write we need to double fallback
+    if [ "x$V" == "x1" ]; then
+        V=`./wolfboot.elf get_version 2>/dev/null`
+    fi
+fi
+
+if [ "x$V" != "x2" ]; then
+    echo "Failed update (V: $V)"
+    exit 1
+fi
+
+echo Test successful.
+exit 0
