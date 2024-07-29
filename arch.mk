ca09a1ab99909cc053b5480c0c03c8fb1c68656c
Daniel Fedai Larsen <daniel@aminacharging.com>
2024-07-29 07:48:20 +0200

    Add HAB support irrespective of MCUSDK setting for imx_rt target

--- arch.mk
+++ arch.mk
@@ -566,12 +566,13 @@ ifeq ($(TARGET),imx_rt)
     ifeq ($(DEBUG_UART),1)
       OBJS+= $(MCUXPRESSO_DRIVERS)/drivers/fsl_lpuart.o
     endif
-    ifeq ($(TARGET_IMX_HAB),1)
-        LSCRIPT_IN:=hal/$(TARGET)_hab.ld
-    else
-        LSCRIPT_IN:=hal/$(TARGET).ld
-    endif
-endif
+  endif
+
+  ifeq ($(TARGET_IMX_HAB),1)
+      LSCRIPT_IN:=hal/$(TARGET)_hab.ld
+  else
+      LSCRIPT_IN:=hal/$(TARGET).ld
+  endif
 
   ifeq ($(MCUXPRESSO_CPU),MIMXRT1064DVL6A)
     ARCH_FLASH_OFFSET=0x70000000
