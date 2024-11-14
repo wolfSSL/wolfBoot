0791d76d653365107813fadc8ba980155e645665
Daniele Lacamera <root@danielinux.net>
2024-11-14 15:44:22 +0100

    Draft: support for rp2350

---
+++ hal/rp2350.c
@@ -0,0 +1,80 @@
+/* rp2350.c
+ *
+ * Stubs for custom HAL implementation. Defines the 
+ * functions used by wolfboot for a specific target.
+ *
+ * Copyright (C) 2021 wolfSSL Inc.
+ *
+ * This file is part of wolfBoot.
+ *
+ * wolfBoot is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfBoot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
+ */
+
+#include <stdint.h>
+#include <target.h>
+#include "image.h"
+
+
+#define SIO_BASE (0xD0000000U)
+#define SIO_CPUID (*(volatile uint32_t *)(SIO_BASE + 0x00))
+
+const uint32_t VTOR_ADDR = 0xE000ED08;
+const uint32_t BOOTROM_VTABLE_OFFSET = 0x00000000;
+
+void __attribute__((naked)) jmp_to_rom_vector(void)
+{
+    __asm volatile(
+        "ldr r0, =BOOTROM_VTABLE_OFFSET\n"
+        "ldr r1, =VTOR_ADDR\n"
+        "str r0, [r1]\n"
+        "ldmia r0!, {r1, r2}\n"
+        "msr msp, r1\n"
+        "bx r2\n"
+    );
+}
+
+#ifdef __WOLFBOOT
+void hal_init(void)
+{
+    /* Keep CPU1 in ROM */
+    if (SIO_CPUID != 0) {
+        jmp_to_rom_vector();
+    }
+}
+
+void hal_prepare_boot(void)
+{
+}
+
+#endif
+
+int RAMFUNCTION hal_flash_write(uint32_t address, const uint8_t *data, int len)
+{
+    return 0; /* on success. */
+}
+
+void RAMFUNCTION hal_flash_unlock(void)
+{
+}
+
+void RAMFUNCTION hal_flash_lock(void)
+{
+}
+
+int RAMFUNCTION hal_flash_erase(uint32_t address, int len)
+{
+    return 0; /* on success. */
+}
+
