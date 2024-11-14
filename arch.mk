77fb63dbc95b6f832afd4f7199d40b47da138d89
Daniele Lacamera <root@danielinux.net>
2024-11-14 16:05:22 +0100

    arch.mk: rp2350

--- arch.mk
+++ arch.mk
@@ -211,6 +211,20 @@ ifeq ($(ARCH),ARM)
     SPI_TARGET=stm32
   endif
 
+  ifeq ($(TARGET),rp2350)
+    CORTEX_M33=1
+    CFLAGS+=-Ihal
+    ARCH_FLASH_OFFSET=0x10000000
+    WOLFBOOT_ORIGIN=0x10000000
+    ifeq ($(TZEN),1)
+      LSCRIPT_IN=hal/$(TARGET).ld
+    else
+      LSCRIPT_IN=hal/$(TARGET)-ns.ld
+    endif
+    SPI_TARGET=raspberrypi_pico
+    CFLAGS+=-DPICO_SDK_PATH=$(PICO_SDK_PATH)
+  endif
+
   ifeq ($(TARGET),sama5d3)
      CORTEX_A5=1
      UPDATE_OBJS:=src/update_ram.o
@@ -1070,7 +1084,7 @@ ifeq ($(TARGET),sim)
     CFLAGS+=-DWOLFSSL_SP_DIV_WORD_HALF
   endif
   ifeq ($(WOLFHSM_CLIENT),1)
-	  WOLFHSM_CLIENT_OBJS += $(LIBDIR)/wolfHSM/port/posix/posix_transport_tcp.o
+    WOLFHSM_CLIENT_OBJS += $(LIBDIR)/wolfHSM/port/posix/posix_transport_tcp.o
   endif
 endif
 
