8c2bae81ecabce36e24098451c0c398e2e74300b
Daniele Lacamera <dan@danielinux.net>
2024-12-05 13:37:07 +0100

    Test application distributed off-tree

--- IDE/pico-sdk/rp2350/test-app/build-signed-app.sh
+++ IDE/pico-sdk/rp2350/test-app/build-signed-app.sh
@@ -5,6 +5,10 @@ cd build
 cmake .. -DPICO_SDK_PATH=$PICO_SDK_PATH -DPICO_PLATFORM=rp2350
 cat pico_flash_region.ld | sed -e "s/0x10000000/0x10040400/g" >pico_flash_region_wolfboot.ld
 cp pico_flash_region_wolfboot.ld pico_flash_region.ld
+
+# Get off-tree source file from raspberry pico-examples
+curl -o blink.c https://raw.githubusercontent.com/raspberrypi/pico-examples/refs/tags/sdk-2.1.0/blink/blink.c
+
 make clean && make
 ../../../../../tools/keytools/sign --sha256 --ecc256 blink.bin \
     ../../../../../wolfboot_signing_private_key.der 1
