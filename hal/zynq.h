7cf327f7eb2bf2329a127a9fc1b9f76159146277
David Garske <david@wolfssl.com>
2025-01-30 16:39:53 -0800

    Added Xilinx UltraScale+ MPSoC CSU Support CSU support. Enabled support for offloading SHA3 hashing to CSU hardware using PKA=1. Added support for enabling JTAG at runtime if CSU_DEBUG is set. Requires patching PMUFW to enable register access. See: https://xilinx-wiki.atlassian.net/wiki/spaces/A/pages/2587197506/Zynq+UltraScale+MPSoC+JTAG+Enable+in+U-Boot

--- hal/zynq.h
+++ hal/zynq.h
@@ -100,16 +100,17 @@
 #define GQSPIDMA_IMR       (*((volatile uint32_t*)(QSPI_BASE + 0x820))) /* DST DMA interrupt mask */
 #define GQSPIDMA_CTRL2     (*((volatile uint32_t*)(QSPI_BASE + 0x824))) /* General DST DMA control register 2 */
 
-#define GQSPI_LPBK_DLY_ADJ_USE_LPBK       (1UL << 5)
-#define GQSPI_LPBK_DLY_ADJ_DIV0(x)        (((x) & 0x7) << 0)
-#define GQSPI_LPBK_DLY_ADJ_DLY1(x)        (((x) & 0x3) << 3)
+#define GQSPI_LPBK_DLY_ADJ_USE_LPBK        (1UL << 5)
+#define GQSPI_LPBK_DLY_ADJ_DIV0(x)         (((x) & 0x7) << 0)
+#define GQSPI_LPBK_DLY_ADJ_DLY1(x)         (((x) & 0x3) << 3)
 #define GQSPI_DATA_DLY_ADJ_USE_DATA_DLY    (1UL << 31)
 #define GQSPI_DATA_DLY_ADJ_DATA_DLY_ADJ(x) (((x) & 0x7) << 28)
 
 /* GQSPI Registers */
 /* GQSPI_CFG: Configuration registers */
-#define GQSPI_CFG_CLK_POL             (1UL << 1) /* Clock polarity outside QSPI word: 0: QSPI clock is quiescent low, 1: QSPI clock is quiescent high */
-#define GQSPI_CFG_CLK_PH              (1UL << 2) /* Clock phase: 1: the QSPI clock is inactive outside the word, 0: the QSPI clock is active outside the word */
+/* Clock Phase and Polarity. Only mode 3 and 0 are support (11b or 00b) */
+#define GQSPI_CFG_CLK_POL             (1UL << 1) /* Clock polarity: 1: QSPI clock is quiescent high,            0: QSPI clock is quiescent low,                  */
+#define GQSPI_CFG_CLK_PH              (1UL << 2) /* Clock phase:    1: QSPI clock is inactive outside the word, 0: QSPI clock is active outside the word */
 /* 000: divide by 2,   001: divide by 4,  010: divide by 8,
    011: divide by 16,  100: divide by 32, 101: divide by 64,
    110: divide by 128, 111: divide by 256 */
@@ -139,8 +140,8 @@
 
 #define GQSPI_IXR_ALL_MASK (GQSPI_IXR_POLL_TIME_EXPIRE | GQSPI_IXR_TX_FIFO_NOT_FULL | \
     GQSPI_IXR_TX_FIFO_FULL | GQSPI_IXR_RX_FIFO_NOT_EMPTY | GQSPI_IXR_RX_FIFO_FULL | \
-    GQSPI_IXR_GEN_FIFO_EMPTY | GQSPI_IXR_TX_FIFO_EMPTY | GQSPI_IXR_GEN_FIFO_NOT_FULL | \
-    GQSPI_IXR_GEN_FIFO_FULL | GQSPI_IXR_RX_FIFO_EMPTY)
+    GQSPI_IXR_TX_FIFO_EMPTY | GQSPI_IXR_GEN_FIFO_NOT_FULL | GQSPI_IXR_GEN_FIFO_FULL | \
+    GQSPI_IXR_RX_FIFO_EMPTY)
 #define GQSPI_ISR_WR_TO_CLR_MASK 0x00000002U
 
 /* GQSPI_GEN_FIFO: FIFO data register */
@@ -175,7 +176,7 @@
 #define GQSPI_FIFO_CTRL_RST_RX_FIFO  (1UL << 2)
 
 /* GQSPIDMA_CTRL */
-#define GQSPIDMA_CTRL_DEF  0x803FFA00UL
+#define GQSPIDMA_CTRL_DEF  0x403FFA00UL
 #define GQSPIDMA_CTRL2_DEF 0x081BFFF8UL
 
 /* GQSPIDMA_STS */
@@ -187,14 +188,15 @@
 #define GQSPIDMA_ISR_ALL_MASK  0xFEU
 
 /* QSPI Configuration (bare-metal only) */
-
 #ifndef GQSPI_CLK_REF
 #define GQSPI_CLK_REF          125000000 /* QSPI Reference Clock */
 #endif
 #ifndef GQSPI_CLK_DIV
 #define GQSPI_CLK_DIV          2 /* (QSPI_REF_CLK (125MHZ) / (2 << DIV) = BUS): 0=DIV2, 1=DIV4, 2=DIV8 */
 #endif
-#define GQSPI_CS_ASSERT_CLOCKS 5 /* CS Setup Time (tCSS) - num of clock cycles foes in IMM */
+#define GQSPI_CS_ASSERT_CLOCKS   5 /* CS Setup Time (tCSS) */
+#define GQSPI_CS_DEASSERT_CLOCKS 4 /* CS Hold Time */
+
 #define GQSPI_FIFO_WORD_SZ     4
 #define QQSPI_DMA_ALIGN        64 /* L1 cache size */
 #ifndef GQSPI_DMA_TMPSZ
@@ -378,6 +380,190 @@
 
 #define GICD_BASE       0xF9010000
 #define GICC_BASE       0xF9020000
+
+
+/* Clock - Full Power Domain */
+#define CRL_APB_BASE  0xFF5E0000UL
+#define QSPI_REF_CTRL (*((volatile uint32_t*)(CRL_APB_BASE + 0x68)))
+
+#define QSPI_REF_CTRL_SRCSEL_MASK   0x7
+#define QSPI_REF_CTRL_SRCSEL(n)     ((n) & QSPI_REF_CTRL_SRCSEL_MASK)
+#define QSPI_REF_CTRL_SRCSEL_IOPLL  QSPI_REF_CTRL_SRCSEL(0)
+#define QSPI_REF_CTRL_SRCSEL_RPLL   QSPI_REF_CTRL_SRCSEL(2)
+#define QSPI_REF_CTRL_SRCSEL_DPLL   QSPI_REF_CTRL_SRCSEL(3) /* DPLL_CLK_TO_LPD */
+
+#define QSPI_REF_CTRL_DIVISOR0_MASK (0x3F << 8)
+#define QSPI_REF_CTRL_DIVISOR0(n)   (((n) << 8) & QSPI_REF_CTRL_DIVISOR0_MASK)
+
+#define QSPI_REF_CTRL_DIVISOR1_MASK (0x3F << 16)
+#define QSPI_REF_CTRL_DIVISOR1(n)   (((n) << 16) & QSPI_REF_CTRL_DIVISOR0_MASK)
+
+
+/* Configuration Security Unit (CSU) */
+/* Triple-Dedundant MicroBlaze processor */
+/* 128 KB CSU ROM (immutable) */
+/* 32 KB CSU RAM (with ECC) */
+/* Internal clock source */
+/* CSU must be called through PMUFW. */
+/* PMUFW must be built with SECURE_ACCESS_VAL=1 */
+#define CSU_BASE          0xFFCA0000UL
+
+#define CSU_STATUS        (CSU_BASE + 0x0000U)
+#define CSU_STATUS_BOOT_ENC  (1 << 1)
+#define CSU_STATUS_BOOT_AUTH (1 << 0)
+
+/* See JTAG IDCODE in TRM */
+#define CSU_IDCODE        (CSU_BASE + 0x0040U)
+/* 2473_8093h=ZU9EG */
+/* 1471_1093h=ZU2CG/EG */
+
+#define CSU_VERSION       (CSU_BASE + 0x0044U)
+/* 0: XCZU9EG-ES1,
+ * 1: XCZU3EG-ES1, XCZU15EG-ES1,
+ * 2: XCZU7EV-ES1, XCZU9EG-ES2, XCZU19EG-ES1,
+ * 3: All devices as of October 2017 (Production Level) */
+#define CSU_VERSION_MASK  0xF
+
+#define CSU_TAMPER_STATUS (CSU_BASE + 0x5000U)
+#define CSU_TAMPER_TRIG   (CSU_BASE + 0x0014U) /* set =1 to trigger tamber event for testing */
+
+/* SSS - Secure Stream Switch */
+#define CSU_SSS_CFG           (CSU_BASE + 0x0008U)
+#define CSU_SSS_CFG_PCAP_MASK 0x0000000FU
+#define CSU_SSS_CFG_PCAP(n)   (((n) << 0) & CSU_SSS_CFG_PCAP_SSS_MASK)
+#define CSU_SSS_CFG_DMA_MASK  0x000000F0U
+#define CSU_SSS_CFG_DMA(n)    (((n) << 4) & CSU_SSS_CFG_DMA_SSS_MASK)
+#define CSU_SSS_CFG_AES_MASK  0x00000F00U
+#define CSU_SSS_CFG_AES(n)    (((n) << 8) & CSU_SSS_CFG_AES_SSS_MASK)
+#define CSU_SSS_CFG_SHA_MASK  0x0000F000U
+#define CSU_SSS_CFG_SHA(n)    (((n) << 12) & CSU_SSS_CFG_SHA_SSS_MASK)
+/* Data Sources */
+#define CSU_SSS_CFG_SRC_NONE  0x0
+#define CSU_SSS_CFG_SRC_PCAP  0x3 /* Processor Configuration Access Port */
+#define CSU_SSS_CFG_SRC_DMA   0x5
+#define CSU_SSS_CFG_SRC_AES   0xA
+
+/* AES-GCM 256-bit */
+#define CSU_AES_STATUS    (CSU_BASE + 0x1000U)
+#define CSU_AES_KEY_SRC   (CSU_BASE + 0x1004U) /* AES key source selection */
+#define CSU_AES_KEY_LOAD  (CSU_BASE + 0x1008U) /* Loads the key selected by AES_KEY_SRC into the AES (self clearing) */
+#define CSU_AES_START_MSG (CSU_BASE + 0x100CU) /* Starts the decryption process. The IV must be loaded before the AES will decrypt a payload (self clearing) */
+#define CSU_AES_RESET     (CSU_BASE + 0x1010U)
+#define CSU_AES_KEY_CLEAR (CSU_BASE + 0x1014U)
+#define CSU_AES_CFG       (CSU_BASE + 0x1018U) /* 0=Dec, 1=Enc */
+#define CSU_AES_KUP_WR    (CSU_BASE + 0x101CU)
+#define CSU_AES_KUP       (CSU_BASE + 0x1020U) /* 32 bytes - through 0x40 */
+#define CSU_AES_IV        (CSU_BASE + 0x1040U) /* 16 bytes - through 0x50 */
+
+#define CSU_AES_STATUS_OKR_ZEROED     (1 << 11)
+#define CSU_AES_STATUS_BOOT_ZEROED    (1 << 10)
+#define CSU_AES_STATUS_KUP_ZEROED     (1 << 9)
+#define CSU_AES_STATUS_AES_KEY_ZEROED (1 << 8)
+#define CSU_AES_STATUS_KEY_INIT_DONE  (1 << 4)
+#define CSU_AES_STATUS_GCM_TAG_PASS   (1 << 3)
+#define CSU_AES_STATUS_DONE           (1 << 2)
+#define CSU_AES_STATUS_READY          (1 << 1)
+#define CSU_AES_STATUS_BUSY           (1 << 0)
+
+#define CSU_AES_KEY_SRC_DEVICE_KEY    1 /* Device key is selected and locked by the CSU ROM during boot */
+#define CSU_AES_KEY_SRC_KUP           0 /* User provided key source */
+
+#define CSU_AES_KEY_CLEAR_KUP         (1 << 1) /* Zeroize KUP key */
+#define CSU_AES_KEY_CLEAR_EXP         (1 << 0) /* Zeroize expanded key */
+
+/* PUF */
+#define CSU_PUF_CMD     (CSU_BASE + 0x4000U)
+#define CSU_PUF_CFG0    (CSU_BASE + 0x4004U)
+#define CSU_PUF_CFG1    (CSU_BASE + 0x4008U)
+#define CSU_PUF_SHUTTER (CSU_BASE + 0x400CU)
+#define CSU_PUF_STATUS  (CSU_BASE + 0x4010U)
+#define CSU_PUF_WORD    (CSU_BASE + 0x4018U)
+
+#define CSU_PUF_CMD_CLEAR        0x6 /* Clear PUF status */
+#define CSU_PUF_CMD_STATUS       0x5 /* Read out regeneration status */
+#define CSU_PUF_CMD_REGENERATION 0x4 /* Key regeneration */
+#define CSU_PUF_CMD_REGISTRATION 0x1 /* Key registration */
+
+#define CSU_PUF_CFG0_INIT        0x2
+#define CSU_PUF_CFG1_INIT        0x0C230090 /* 0x00080080 */
+#define CSU_PUF_SHUTTER_INIT     0x0100005E /* 0x01000100 */
+
+#define CSU_PUF_STATUS_OVERFLOW_MASK    0x30000000 /* Overflow, if bits are not 0. Reduce SHUT[SOPEN] value. */
+#define CSU_PUF_STATUS_AUX_MASK         0x0FFFFFF0 /* During provisioning, auxiliary sundrome bits are stored here and must be written to the eFuse or boot image. */
+#define CSU_PUF_STATUS_KEY_RDY_MASK     0x00000008
+#define CSU_PUF_STATUS_KEY_ZERO_MASK    0x00000002 /* Indicates that the PUF key has been zeroized */
+#define CSU_PUF_STATUS_SYN_WRD_RDY_MASK 0x00000001 /* Indicates a syndrome word is ready in the PUF_WORD register. */
+
+/* SHA3 */
+#define CSU_SHA_START  (CSU_BASE + 0x2000U)
+#define CSU_SHA_RESET  (CSU_BASE + 0x2004U)
+#define CSU_SHA_DONE   (CSU_BASE + 0x2008U)
+#define CSU_SHA_DIGEST (CSU_BASE + 0x2010U) /* 48 bytes (through 0x40) */
+#define CSU_SHA_START  (CSU_BASE + 0x2000U)
+#define CSU_SHA_START  (CSU_BASE + 0x2000U)
+
+/* CSU DMA */
+/* Addresses and sizes must be word aligned last two bits = 0 */
+/* 128 x 32-bit data FIFO for each channel (two channels) */
+#define CSUDMA_BASE(ch)     (0xFFC80000UL * (((ch) & 0x1) * 0x800))
+#define CSUDMA_ADDR(ch)     (CSUDMA_BASE(ch) + 0x0000U) /* Mem address (lower 32-bits) */
+#define CSUDMA_ADDR_MSB(ch) (CSUDMA_BASE(ch) + 0x0028U) /*             (upper 17 bits) */
+#define CSUDMA_SIZE(ch)     (CSUDMA_BASE(ch) + 0x0004U) /* DMA transfer payload size */
+#define CSUDMA_STS(ch)      (CSUDMA_BASE(ch) + 0x0008U)
+#define CSUDMA_CTRL(ch)     (CSUDMA_BASE(ch) + 0x000CU)
+#define CSUDMA_CTRL2(ch)    (CSUDMA_BASE(ch) + 0x0024U)
+#define CSUDMA_CRC(ch)      (CSUDMA_BASE(ch) + 0x0010U)
+#define CSUDMA_ISTS(ch)     (CSUDMA_BASE(ch) + 0x0014U)
+#define CSUDMA_IEN(ch)      (CSUDMA_BASE(ch) + 0x0018U)
+#define CSUDMA_IDIS(ch)     (CSUDMA_BASE(ch) + 0x001CU)
+#define CSUDMA_IMASK(ch)    (CSUDMA_BASE(ch) + 0x0020U)
+
+#define CSUDMA_STS_DONE_CNT       (0x07 << 13)
+#define CSUDMA_STS_SRC_FIFO_LEVEL (0xFF << 5)
+#define CSUDMA_STS_RD_OUTSTANDING (0x0F << 1)
+#define CSUDMA_STS_BUSY           (0x01 << 0)
+
+#define CSUDMA_CTRL_FIFO_THRESH_MASK (0xF << 2)
+#define CSUDMA_CTRL_FIFO_THRESH(n)   (((n) << 2) & CSUDMA_CTRL_FIFO_THRESH_MASK)
+#define CSUDMA_CTRL_TIMEOUT_VAL_MASK (0xFFF << 10)
+#define CSUDMA_CTRL_TIMEOUT_VAL(n)   (((n) << 10) & CSUDMA_CTRL_TIMEOUT_VAL_MASK)
+#define CSUDMA_CTRL_ENDIANNESS       (1 << 23)
+#define CSUDMA_CTRL_AXI_BRST_TYPE    (1 << 22)
+
+#define CSUDMA_CTRL2_ARCACHE_MASK       (0x7 << 24)
+#define CSUDMA_CTRL2_ARCACHE(n)         (((n) << 24) & CSUDMA_CTRL2_ARCACHE_MASK)
+#define CSUDMA_CTRL2_ROUTE_BIT          (1 << 23)
+#define CSUDMA_CTRL2_TIMEOUT_EN         (1 << 22)
+#define CSUDMA_CTRL2_TIMEOUT_PRE_MASK   (0xFFF << 4)
+#define CSUDMA_CTRL2_TIMEOUT_PRE(n)     (((n) << 4) & CSUDMA_CTRL2_TIMEOUT_PRE_MASK)
+#define CSUDMA_CTRL2_MAX_OUTS_CMDS_MASK (0xF << 0)
+#define CSUDMA_CTRL2_MAX_OUTS_CMDS(n)   (((n) << 0) & CSUDMA_CTRL2_MAX_OUTS_CMDS_MASK)
+
+#define CSUDMA_ISR_INVALID_APB  (1 << 6)
+#define CSUDMA_ISR_THRESH_HIT   (1 << 5)
+#define CSUDMA_ISR_TIMEOUT_MEM  (1 << 4)
+#define CSUDMA_ISR_TIMEOUT_STRM (1 << 3)
+#define CSUDMA_ISR_AXI_RDERR    (1 << 2)
+#define CSUDMA_ISR_DONE         (1 << 1)
+#define CSUDMA_ISR_MEM_DONE     (1 << 0)
+
+/* CSU DMA Channels */
+#define CSUDMA_CH_SRC 0
+#define CSUDMA_CH_DST 1
+
+/* CSU JTAG */
+#define CSU_JTAG_CHAIN_CFG    (CSU_BASE + 0x0030U)
+#define CSU_JTAG_CHAIN_STATUS (CSU_BASE + 0x0034U)
+#define CSU_JTAG_SEC          (CSU_BASE + 0x0038U)
+#define CSU_JTAG_DAP_CFG      (CSU_BASE + 0x003CU)
+
+#define CSU_PCAP_PROG         (CSU_BASE + 0x3000U)
+
+/* Clock and Reset Control */
+#define CRL_APB_BASE 0xFF5E0000UL
+#define CRL_APB_DBG_LPD_CTRL (CRL_APB_BASE + 0x00B0U)
+#define CRL_APB_RST_LPD_DBG  (CRL_APB_BASE + 0x0240U)
+
 
 
 #endif /* _ZYNQMP_H_ */
