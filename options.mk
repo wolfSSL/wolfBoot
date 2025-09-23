6cdf76b54e40de57e3357f1a510611c173b1019f
Daniele Lacamera <root@danielinux.net>
2025-09-23 17:54:29 +0200

    Fix malloc in pkcs11_store

--- options.mk
+++ options.mk
@@ -652,6 +652,7 @@ ifeq ($(WOLFCRYPT_TZ_PKCS11),1)
   CFLAGS+=-DCK_CALLABLE="__attribute__((cmse_nonsecure_entry))"
   CFLAGS+=-Ilib/wolfPKCS11
   CFLAGS+=-DWP11_HASH_PIN_COST=3
+  LDFLAGS+=--specs=nano.specs
   WOLFCRYPT_OBJS+=src/pkcs11_store.o
   WOLFCRYPT_OBJS+=src/pkcs11_callable.o
   WOLFCRYPT_OBJS+=./lib/wolfssl/wolfcrypt/src/pwdbased.o
