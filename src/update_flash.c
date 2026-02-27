d7c290ad3aaf524c6757723c4614f0ae2318fa7d
Thomas Cook <thomas.w.cook.ii@gmail.com>
2026-02-27 01:29:51 -0500

    Deploy NO_DIRECT_READ_OF_ERASED_SECTOR to protect against hardfault with AHB read of erased sector.

--- src/update_flash.c
+++ src/update_flash.c
@@ -336,7 +336,12 @@ static int RAMFUNCTION wolfBoot_swap_and_final_erase(int resume)
     ext_flash_read((uintptr_t)(boot->hdr + tmpBootPos), (void*)tmpBuffer,
         sizeof(tmpBuffer));
 #else
-    memcpy(tmpBuffer, boot->hdr + tmpBootPos, sizeof(tmpBuffer));
+# ifdef NO_DIRECT_READ_OF_ERASED_SECTOR
+    if (hal_flash_is_erased_at((uintptr_t)(boot->hdr + tmpBootPos)))
+        memset(tmpBuffer, 0xFF, sizeof(tmpBuffer));
+    else
+# endif
+        memcpy(tmpBuffer, boot->hdr + tmpBootPos, sizeof(tmpBuffer));
 #endif
 
     /* Check if the magic trailer exists - indicates an interrupted swap
