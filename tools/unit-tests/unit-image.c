dc2664a7011ed7b63a9dfba907d8d4d44b39aabf
Brett Nicholas <7547222+bigbrett@users.noreply.github.com>
2026-04-14 12:43:02 -0600

    update unit tests to use new APIs

--- tools/unit-tests/unit-image.c
+++ tools/unit-tests/unit-image.c
@@ -361,7 +361,9 @@ uint16_t wolfBoot_find_header(uint8_t *haystack, uint16_t type, uint8_t **ptr)
 }
 
 #if defined(WOLFBOOT_SIGN_ECC256)
-int wc_ecc_init(ecc_key* key) {
+int wc_ecc_init_ex(ecc_key* key, void* heap, int devId) {
+    (void)heap;
+    (void)devId;
     if (ecc_init_fail)
         return -1;
     return 0;
@@ -460,10 +462,11 @@ END_TEST
     defined(WOLFBOOT_SIGN_RSA4096) || defined(WOLFBOOT_SIGN_SECONDARY_RSA2048) || \
     defined(WOLFBOOT_SIGN_SECONDARY_RSA3072) || \
     defined(WOLFBOOT_SIGN_SECONDARY_RSA4096)
-int wc_InitRsaKey(RsaKey* key, void* heap)
+int wc_InitRsaKey_ex(RsaKey* key, void* heap, int devId)
 {
     (void)key;
     (void)heap;
+    (void)devId;
     return 0;
 }
 
