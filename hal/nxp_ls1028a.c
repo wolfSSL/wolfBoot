66cb9a6949167939af55dbca300cc68823b9a461
Mattia Moffa <mattia@moffa.xyz>
2026-06-03 18:07:02 +0200

    Layerscape 1028a: fix pointer arithmetic errors
    Fixes F#5345

--- hal/nxp_ls1028a.c
+++ hal/nxp_ls1028a.c
@@ -553,7 +553,8 @@ void xspi_flash_write(uintptr_t address, const uint8_t *data, uint32_t len)
             while (!(XSPI_INTR & XSPI_INTR_IPTXWE_MASK));
 
             for(j = 0; j < XSPI_IP_WM_SIZE; j+=4) {
-                memcpy(&tx_data, data++, 4);
+                memcpy(&tx_data, data, 4);
+                data += 4;
                 xspi_writereg((uint32_t*)XSPI_TFD_BASE + j, tx_data);
             }
 
@@ -570,8 +571,9 @@ void xspi_flash_write(uintptr_t address, const uint8_t *data, uint32_t len)
 
             for(j = 0; j < remaining; j+=4) {
                 tx_data = 0;
-                rem_size = (remaining < 4) ? remaining : 4;
-                memcpy(&tx_data, data++, rem_size);
+                rem_size = ((remaining - j) < 4) ? (remaining - j) : 4;
+                memcpy(&tx_data, data, rem_size);
+                data += rem_size;
                 xspi_writereg((uint32_t*)XSPI_TFD_BASE + j, tx_data);
             }
 
