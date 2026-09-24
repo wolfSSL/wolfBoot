c7bf895cc8b6f600b000f96cb127c52dad7fabfe
Daniele Lacamera <root@danielinux.net>
2026-09-24 10:56:40 +0200

    F-12886: reject non-positive length in HiFive1 flash erase
    For len <= 0 the inclusive end (address + len - 1) underflows and
    the sector loop runs through address wraparound. Treat a
    non-positive length as a no-op before computing the end.

--- hal/hifive1.c
+++ hal/hifive1.c
@@ -573,8 +573,14 @@ int RAMFUNCTION hal_flash_erase(uint32_t address, int len)
 {
     uint32_t end;
     uint32_t p;
-    if (address >= FLASH_BASE)
+
+    /* A non-positive length would underflow the inclusive end below. */
+    if (len <= 0) {
+        return 0;
+    }
+    if (address >= FLASH_BASE) {
         address -= FLASH_BASE;
+    }
     end = address + len - 1;
 
     FESPI_REG_TXMARK = 1;
